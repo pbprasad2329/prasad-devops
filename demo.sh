ab7c810ae8edf8227dc832071576043e708d8489
pbprasad2329 <pbprasad2329@gmail.com>
2022-11-25 05:26:24 +0530

    update

---
+++ demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "***********Display root F.S space"
+df -h
+
+echo "***********Display today date****"
+date
+
+echo "***********Display jenkins memory space ***********"
+free -m
