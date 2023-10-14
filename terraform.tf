f1133034fc96735df1b634aba55eb08dc11dc9e2
support@v24.network <pbprasad2329@gmail.com>
2023-10-14 10:16:18 +0530

    tf file

--- terraform.tf
+++ terraform.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "instance_sg" {
 
 # Create public EC2 instance
 resource "aws_instance" "public_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (change this to your desired AMI)
+  ami           = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 AMI (change this to your desired AMI)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = "your-key-pair-name"  # Change this to your SSH key pair
